03d798d511e2859f1197b9a18a1d0d6719d725af
Trace <tracedelange@me.com>
2021-08-17 13:20:02 -0500

    commit tree 356361d2e37530448cba96aaf75293e2b9fa912e

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,13 @@
 require 'pry'
+# require_relative('./concerns/memorable')
 
 class Artist
+
+  extend Memorable
+  extend Findable
+
+  include Paramable
+
   attr_accessor :name
   attr_reader :songs
 
@@ -11,20 +18,8 @@ class Artist
     @songs = []
   end
 
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
-
   def self.all
     @@artists
-  end
-
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
   end
 
   def add_song(song)
@@ -36,7 +31,7 @@ class Artist
     songs.each { |song| add_song(song) }
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  # def to_param
+  #   name.downcase.gsub(' ', '-')
+  # end
 end
